41fe08993f32fb127d0f0aec663e62928da7df3e
Ángel Reyes <destopline555@hotmail.com>
2024-02-12 14:59:47 -0400

    Add syntax related plugins

---
+++ lua/plugins/syntax.lua
@@ -0,0 +1,17 @@
+return {
+  { "tpope/vim-sleuth" },
+
+  { "hiphish/rainbow-delimiters.nvim" },
+
+  { "jiangmiao/auto-pairs" },
+
+  { "numToStr/Comment.nvim", opts = {} },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = "ibl",
+    opts = {},
+  },
+}
