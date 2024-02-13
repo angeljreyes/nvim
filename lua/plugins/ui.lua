2b1878036ed9a33f20d0a693b6e7b44afe160a22
Ángel Reyes <destopline555@hotmail.com>
2024-02-12 22:30:43 -0400

    Add ui related plugins

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,13 @@
+return {
+  { "nvim-tree/nvim-web-devicons" },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+      },
+    },
+  },
+}
