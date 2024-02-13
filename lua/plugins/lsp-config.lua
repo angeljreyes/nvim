3dddd0016db549558c27cc3f04bf9ef02285572b
Ángel Reyes <destopline555@hotmail.com>
2024-02-12 22:37:26 -0400

    Add LSP related plugins

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,183 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim", config = true },
+    "williamboman/mason-lspconfig.nvim",
+    { "j-hui/fidget.nvim", opts = {} },
+    "folke/neodev.nvim",
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+        -- Snippet Engine & its associated nvim-cmp source
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+
+        -- Adds LSP completion capabilities
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-path",
+
+        -- Adds a number of user-friendly snippets
+        "rafamadriz/friendly-snippets",
+      },
+    },
+  },
+  config = function()
+    local on_attach = function(_, bufnr)
+      local nmap = function(keys, func, desc)
+        if desc then
+          desc = "LSP: " .. desc
+        end
+
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+      end
+
+      nmap("<leader>cr", vim.lsp.buf.rename, "Code Rename")
+      nmap("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+
+      nmap("gd", require("telescope.builtin").lsp_definitions, "Go to Definition")
+      nmap("gr", require("telescope.builtin").lsp_references, "Go to References")
+      nmap("gI", require("telescope.builtin").lsp_implementations, "Go to Implementation")
+      nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "type Definition")
+      nmap("<leader>cs", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
+      nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace Symbols")
+
+      -- See `:help K` for why this keymap
+      nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+      nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+      -- Lesser used LSP functionality
+      nmap("gD", vim.lsp.buf.declaration, "Go to Declaration")
+      nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "Workspace Add Folder")
+      nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "Workspace Remove Folder")
+      nmap("<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, "Workspace List Folders")
+
+      -- Create a command `:Format` local to the LSP buffer
+      vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+        vim.lsp.buf.format()
+      end, { desc = "Format current buffer with LSP" })
+    end
+
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_pending = " ",
+          package_installed = "󰄳 ",
+          package_uninstalled = " 󰚌",
+        },
+      }
+    })
+    require("mason-lspconfig").setup()
+
+    local servers = {
+      -- gopls = {},
+      -- rust_analyzer = {},
+      -- html = { filetypes = { "html", "twig", "hbs"} },
+
+      omnisharp = {
+        filetypes = { "csx", "cs" },
+      },
+      tsserver = {},
+      angularls = {},
+      tailwindcss = {},
+      clangd = {},
+      pyright = {},
+      lua_ls = {
+        Lua = {
+          workspace = { checkThirdParty = false },
+          telemetry = { enable = false },
+          -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+          -- diagnostics = { disable = { "missing-fields" } },
+        },
+      },
+    }
+
+    -- Setup neovim lua configuration
+    require("neodev").setup()
+
+    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+    -- Ensure the servers above are installed
+    local mason_lspconfig = require "mason-lspconfig"
+
+    mason_lspconfig.setup {
+      ensure_installed = vim.tbl_keys(servers),
+    }
+
+    mason_lspconfig.setup_handlers {
+      function(server_name)
+        require("lspconfig")[server_name].setup {
+          capabilities = capabilities,
+          on_attach = on_attach,
+          settings = servers[server_name],
+          filetypes = (servers[server_name] or {}).filetypes,
+        }
+      end,
+    }
+
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+      vim.lsp.diagnostic.on_publish_diagnostics,
+      {
+        virtual_text = true,
+        signs = false,
+        update_in_insert = true,
+        underline = true,
+      }
+    )
+
+    -- [[ Configure nvim-cmp ]]
+    -- See `:help cmp`
+    local cmp = require "cmp"
+    local luasnip = require "luasnip"
+    require("luasnip.loaders.from_vscode").lazy_load()
+    luasnip.config.setup({})
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete {},
+        ["<CR>"] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+      },
+    })
+  end
+}
