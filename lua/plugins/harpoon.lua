f2459ef84852160cd8a85245f2d03a93046347ed
Ángel Reyes <destopline555@hotmail.com>
2024-02-13 23:16:29 -0400

    Make harpoon list dynamic
    Now, when chaning current working directories, harpoon loads the
    previously used list.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -27,24 +27,23 @@ return {
         :find()
     end
 
-    local hp_list = harpoon:list()
-    vim.keymap.set("n", "<leader>as", function() toggle_telescope(hp_list) end, { desc = "Search" })
-    vim.keymap.set("n", "<leader>an", function() hp_list:append() end, { desc = "New index" })
+    vim.keymap.set("n", "<leader>as", function() toggle_telescope(harpoon:list()) end, { desc = "Search" })
+    vim.keymap.set("n", "<leader>an", function() harpoon:list():append() end, { desc = "New index" })
 
-    vim.keymap.set("n", "<leader>aj", function() hp_list:select(1) end, { desc = "First index" })
-    vim.keymap.set("n", "<leader>ak", function() hp_list:select(2) end, { desc = "Second index" })
-    vim.keymap.set("n", "<leader>al", function() hp_list:select(3) end, { desc = "Third index" })
-    vim.keymap.set("n", "<leader>a;", function() hp_list:select(4) end, { desc = "Fourth index" })
+    vim.keymap.set("n", "<leader>aj", function() harpoon:list():select(1) end, { desc = "First index" })
+    vim.keymap.set("n", "<leader>ak", function() harpoon:list():select(2) end, { desc = "Second index" })
+    vim.keymap.set("n", "<leader>al", function() harpoon:list():select(3) end, { desc = "Third index" })
+    vim.keymap.set("n", "<leader>a;", function() harpoon:list():select(4) end, { desc = "Fourth index" })
 
-    vim.keymap.set("n", "<leader>adj", function() hp_list:removeAt(1) end, { desc = "Delete first index" })
-    vim.keymap.set("n", "<leader>adk", function() hp_list:removeAt(2) end, { desc = "Delete second index" })
-    vim.keymap.set("n", "<leader>adl", function() hp_list:removeAt(3) end, { desc = "Delete third index" })
-    vim.keymap.set("n", "<leader>ad;", function() hp_list:removeAt(4) end, { desc = "Delete fourth index" })
+    vim.keymap.set("n", "<leader>adj", function() harpoon:list():removeAt(1) end, { desc = "Delete first index" })
+    vim.keymap.set("n", "<leader>adk", function() harpoon:list():removeAt(2) end, { desc = "Delete second index" })
+    vim.keymap.set("n", "<leader>adl", function() harpoon:list():removeAt(3) end, { desc = "Delete third index" })
+    vim.keymap.set("n", "<leader>ad;", function() harpoon:list():removeAt(4) end, { desc = "Delete fourth index" })
 
-    vim.keymap.set("n", "<leader>ac", function() hp_list:clear() end, { desc = "Clear list" })
+    vim.keymap.set("n", "<leader>ac", function() harpoon:list():clear() end, { desc = "Clear list" })
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-z>", function() hp_list:prev() end, { desc = "Previous harpoon index" })
-    vim.keymap.set("n", "<C-x>", function() hp_list:next() end, { desc = "Next harpoon index" })
+    vim.keymap.set("n", "<C-z>", function() harpoon:list():prev() end, { desc = "Previous harpoon index" })
+    vim.keymap.set("n", "<C-x>", function() harpoon:list():next() end, { desc = "Next harpoon index" })
   end,
 }
