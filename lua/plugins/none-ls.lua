df3fe87e318900e0fb32ce2ee1be0f783d3b6c8c
Ángel Reyes <destopline555@hotmail.com>
2024-02-13 21:11:20 -0400

    Add none-ls plugin

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,20 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    local btins = null_ls.builtins
+    null_ls.setup({
+      sources = {
+        btins.formatting.stylua,
+
+        btins.diagnostics.ruff,
+        btins.formatting.black,
+        btins.formatting.isort,
+
+        btins.diagnostics.eslint_d,
+        btins.formatting.prettier,
+      },
+    })
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format current buffer" })
+  end,
+}
