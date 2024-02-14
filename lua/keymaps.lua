d8f420b1eeb74fd66fd9fd199f4ca5fa791e20ef
Ángel Reyes <destopline555@hotmail.com>
2024-02-13 22:50:44 -0400

    Show diagnostics message window with borders

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,12 +26,22 @@ vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set(
+  "n",
+  "[d",
+  function() vim.diagnostic.goto_prev({ float = { border = "rounded" } }) end,
+  { desc = "Go to previous diagnostic message" }
+)
+vim.keymap.set(
+  "n",
+  "]d",
+  function() vim.diagnostic.goto_next({ float = { border = "rounded" } }) end,
+  { desc = "Go to next diagnostic message" }
+)
 vim.keymap.set(
   "n",
   "<leader>e",
-  vim.diagnostic.open_float,
+  function() vim.diagnostic.open_float({ border = "rounded" }) end,
   { desc = "Open floating diagnostic message" }
 )
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
