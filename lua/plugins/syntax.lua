35769f93ebdb6f6a0834d262f5000b16a908ff23
Ángel Reyes <destopline555@hotmail.com>
2024-03-03 16:29:23 -0400

    Remove auto-pairs CR fix
    It's no longer needed since CR is no longer remapped for cmp

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -3,20 +3,12 @@ return {
 
   { "hiphish/rainbow-delimiters.nvim" },
 
-  {
-    "jiangmiao/auto-pairs",
-    config = function()
-      -- Avoid nasty bug with cmp remapping CR
-      vim.g.AutoPairsMapCR = 0
-    end,
-  },
+  { "jiangmiao/auto-pairs" },
 
   { "numToStr/Comment.nvim", opts = {} },
 
   {
     "lukas-reineke/indent-blankline.nvim",
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = "ibl",
     opts = {},
   },
