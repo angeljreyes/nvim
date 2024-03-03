463293fa532e4be999b2ec721a239cff09aa94fa
Ángel Reyes <destopline555@hotmail.com>
2024-03-03 12:51:42 -0400

    Add todo-comments plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,6 +2,20 @@ return {
   { "nvim-tree/nvim-web-devicons" },
 
   {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local tc = require("todo-comments")
+      tc.setup({
+        signs = false,
+      })
+      vim.keymap.set("n", "]t", function() tc.jump_next() end, { desc = "Next todo comment" })
+      vim.keymap.set("n", "[t", function() tc.jump_prev() end, { desc = "Previous todo comment" })
+      vim.keymap.set("n", "<leader>st", vim.cmd.TodoTelescope, { desc = "Search todo comments" })
+    end,
+  },
+
+  {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
