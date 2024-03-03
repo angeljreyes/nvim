043f47b2e10079e55c1d8b8fa2ff23fbb09ad690
Ángel Reyes <destopline555@hotmail.com>
2024-03-03 16:07:26 -0400

    Change Neo-tree icons

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -10,6 +10,29 @@ return {
       -- See `# Preview Mode` for more information
     },
     config = function()
+      require("neo-tree").setup({
+        default_component_configs = {
+          modified = {
+            symbol = "",
+            highlight = "NeoTreeModified",
+          },
+          git_status = {
+            symbols = {
+              -- Change type
+              added     = "A", -- or "✚", but this is redundant info if you use git_status_colors on the name
+              modified  = "M", -- or "", but this is redundant info if you use git_status_colors on the name
+              deleted   = "D",-- this can only be used in the git_status source
+              renamed   = "R",-- this can only be used in the git_status source
+              -- Status type
+              -- untracked = "",
+              -- ignored   = "",
+              -- unstaged  = "󰄱;",
+              -- staged    = "",
+              -- conflict  = "",
+            },
+          },
+        }
+      })
       vim.keymap.set(
         "n",
         "<leader>f",
