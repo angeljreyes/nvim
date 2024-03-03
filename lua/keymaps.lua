84637ae609226ec605cf6a7f4565373126999cb0
Ángel Reyes <destopline555@hotmail.com>
2024-03-03 14:09:22 -0400

    Remove line wrap jk keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -21,10 +21,6 @@ vim.keymap.set({ "n", "v" }, "Z", "zz", { desc = "Center this line" })
 
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
--- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 vim.filetype.add({
   pattern = {
     ["req.*.txt"] = "requirements",
