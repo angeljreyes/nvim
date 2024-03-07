c6711156a23f76d47d57cca715c0c7b64926f190
Ángel Reyes <destopline555@hotmail.com>
2024-03-07 14:46:09 -0400

    Update toggleterm key mappings

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,18 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  opts = {
-    open_mapping = [[<c-\>]],
-  },
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<c-t>]],
+    })
+    -- Add terminal only keymaps
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+      vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+    end
+
+    -- If you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+  end,
 }
