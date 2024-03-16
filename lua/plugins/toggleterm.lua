2f8602eeb906ef2132b7c1f370532665fcef505d
Ángel Reyes <destopline555@hotmail.com>
2024-03-16 11:03:46 -0400

    Make toggleterm be lazy

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,10 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
+  keys = {
+    { "<C-t>", vim.cmd.ToggleTerm },
+  },
+  cmd = "ToggleTerm",
   config = function()
     require("toggleterm").setup({
       open_mapping = [[<c-t>]],
