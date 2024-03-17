071aeb5fed635f4e5a18935edca258c3533f3ee3
Ángel Reyes <destopline555@hotmail.com>
2024-03-17 00:12:08 -0400

    Add hack to avoid crashing nvim due to presence.nvim

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -1,3 +1,11 @@
 return {
   "andweeb/presence.nvim",
+  -- Enter with `nvim +NoPresence` to avoid loading this plugin. This is
+  -- useful when trying to avoid crashing Neovim when opening more
+  -- than one instance of Neovim at a time.
+  cond = function()
+    vim.api.nvim_create_user_command("NoPresence", "exec", {})
+    vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = "delcommand NoPresence" })
+    return vim.fn.index(vim.v.argv, "+NoPresence") < 0
+  end,
 }
