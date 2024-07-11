03ceb9560528dbc1ee6060414e95694643967bb6
DestopLine <destopline555@hotmail.com>
2024-07-11 19:24:35 -0400

    Fix undotree not working on Windows

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,5 +2,8 @@ return {
   "mbbill/undotree",
   config = function()
     vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree" })
+    if vim.loop.os_uname().sysname == "Windows_NT" then
+      vim.g.undotree_DiffCommand = "FC"
+    end
   end,
 }
