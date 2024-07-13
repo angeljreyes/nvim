f7b9b56f5dd05bd075489c83ecad826e1987df4d
DestopLine <destopline555@hotmail.com>
2024-07-13 17:48:19 -0400

    Use presence.nvim instead of coc-discord-rpc on Linux
    The former would crash on Windows if a second Neovim instance was
    opened, therefore coc.nvim plugin coc-discord-rpc is used as a fallback.

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -1,6 +1,18 @@
+-- presence.nvim crashes on Windows when Neovim is opened more than once
+-- coc-discord-rpc is an alternative that works but requires coc which
+-- requires npm and some extra configuring
+local on_windows = vim.uv.os_uname().sysname == "Windows_NT"
+
 return {
-  "neoclide/coc.nvim",
-  enabled = vim.env.NVIM_PROFILE == "home",
-  branch = "release",
-  build = ":CocInstall coc-discord-rpc",
+  {
+    "neoclide/coc.nvim",
+    enabled = vim.env.NVIM_PROFILE == "home" and on_windows,
+    branch = "release",
+    build = ":CocInstall coc-discord-rpc",
+  },
+
+  {
+    "andweeb/presence.nvim",
+    enabled = vim.env.NVIM_PROFILE == "home" and not on_windows,
+  },
 }
