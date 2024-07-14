19fc6b1adf67d7127ff33d24e7f1e48c595c92f4
DestopLine <destopline555@hotmail.com>
2024-07-14 19:13:20 -0400

    Add keymaps for Telescope git_commits and git_bcommits

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -146,6 +146,8 @@ return {
       vim.keymap.set("n", "<leader>sd", tcbi.diagnostics, { desc = "Search Diagnostics" })
       vim.keymap.set("n", "<leader>sr", tcbi.resume, { desc = "Search Resume" })
       vim.keymap.set("n", "<leader>sk", tcbi.keymaps, { desc = "Search Keymaps" })
+      vim.keymap.set("n", "<leader>gc", tcbi.git_bcommits, { desc = "Git Commits on current buffer" })
+      vim.keymap.set("n", "<leader>gC", tcbi.git_commits, { desc = "Git Commits" })
     end,
   },
 
