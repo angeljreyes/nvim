d6ea5792ed58b13a04dcd5218d3e2e8715b017bb
DestopLine <destopline555@hotmail.com>
2024-08-04 23:29:05 -0400

    Add my own plugin boilersharp.nvim

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -1,64 +1,81 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-
-    -- Adds LSP completion capabilities
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
-
-    -- Adds a number of user-friendly snippets
-    "rafamadriz/friendly-snippets",
+  {
+    "DestopLine/boilersharp.nvim",
+    dev = false,
+    dependencies = {
+      "L3MON4D3/LuaSnip"
+    },
+    config = function()
+      require("boilersharp").setup({
+        add_usings = "csproj",
+        use_file_scoped_namespaces = "csproj",
+        auto_mode = "on_empty",
+      })
+    end,
   },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    require("luasnip.loaders.from_vscode").lazy_load()
-    luasnip.config.setup({})
-    luasnip.filetype_extend("angular", { "html" })
-    luasnip.filetype_extend("razor", { "html", "cs" })
 
-    cmp.setup({
-      snippet = {
-        expand = function(args) luasnip.lsp_expand(args.body) end,
-      },
-      completion = {
-        completeopt = "menu,menuone,noinsert",
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-k>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-j>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete({}),
-        ["<C-l>"] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { "i", "s" }),
-        ["<C-h>"] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { "i", "s" }),
-      }),
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "path" },
-        { name = "lazydev", group_index = 0 },
-      },
-    })
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
 
-    if vim.env.NVIM_PROFILE ~= "home" then
-      require("thiswillbeaplugin").setup()
-    end
-  end,
+      -- Adds LSP completion capabilities
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+
+      -- Adds a number of user-friendly snippets
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      require("luasnip.loaders.from_vscode").lazy_load()
+      luasnip.config.setup({})
+      luasnip.filetype_extend("angular", { "html" })
+      luasnip.filetype_extend("razor", { "html", "cs" })
+
+      cmp.setup({
+        snippet = {
+          expand = function(args) luasnip.lsp_expand(args.body) end,
+        },
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-j>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete({}),
+          ["<C-l>"] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { "i", "s" }),
+          ["<C-h>"] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { "i", "s" }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+          { name = "lazydev", group_index = 0 },
+        },
+      })
+
+      if vim.env.NVIM_PROFILE ~= "home" then
+        require("thiswillbeaplugin").setup()
+      end
+    end,
+  },
 }
