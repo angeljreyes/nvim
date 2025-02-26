8ca2b3c54cb136f543582cfe33807c6bb4691e7f
DestopLine <destopline555@hotmail.com>
2025-02-26 18:49:55 -0400

    Move none-ls to lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,7 +51,7 @@ local on_lsp_attach = function(client, bufnr)
 end
 
 return {
-  { "j-hui/fidget.nvim",    config = true },
+  { "j-hui/fidget.nvim", config = true },
 
   {
     "williamboman/mason.nvim",
@@ -171,6 +171,36 @@ return {
         { path = "luvit-meta/library", words = { "vim%.uv" } },
       },
     },
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      local btins = null_ls.builtins
+      null_ls.setup({
+        sources = {
+          btins.formatting.csharpier,
+          btins.formatting.stylua,
+          btins.formatting.isort,
+          btins.formatting.prettier.with({
+            extra_filetypes = { "angular" },
+          }),
+        },
+      })
+
+      vim.keymap.set({ "n", "x" }, "<leader>cf", function()
+        local clients = vim.lsp.get_clients({ bufnr = 0 })
+        local is_null_ls_attached = vim.iter(clients):any(function(client) return client.name == "null-ls" end)
+        if is_null_ls_attached then
+          vim.lsp.buf.format({ name = "null-ls" })
+        else
+          vim.lsp.buf.format()
+        end
+      end, { desc = "Format current buffer" })
+
+      vim.diagnostic.config({ signs = false })
+    end,
   },
 
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
