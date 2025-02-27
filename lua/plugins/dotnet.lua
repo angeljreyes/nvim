ae0e07efde8c667f5ed88effd6c86fa5a788f9ef
DestopLine <destopline555@hotmail.com>
2025-02-26 20:11:16 -0400

    Completely remove telescope

--- lua/plugins/dotnet.lua
+++ lua/plugins/dotnet.lua
@@ -7,11 +7,12 @@ return {
 
   {
     "GustavEikaas/easy-dotnet.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local dotnet = require("easy-dotnet")
 
       dotnet.setup({
+        picker = "basic",
         test_runner = {
           viewmode = "float",
           mappings = {
