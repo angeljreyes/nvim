d4076d3323f2921e78a90ac75185d045ba9781fd
DestopLine <destopline555@hotmail.com>
2025-03-30 16:35:56 -0400

    Move which-key to ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -43,6 +43,24 @@ return {
   },
 
   {
+    "folke/which-key.nvim",
+    opts = {
+      ---@module "which-key"
+      ---@type wk.Spec
+      spec = {
+        { "<leader>a", group = "Harpoon" },
+        { "<leader>c", group = "Code" },
+        { "<leader>d", group = "Debugger" },
+        { "<leader>h", group = "Git hunk" },
+        { "<leader>s", group = "Search" },
+        { "<leader>.", group = "Open scratch window" },
+        { "<leader>", group = "Visual <leader>", mode = "v" },
+        { "<leader>h", group = "Git hunk", mode = "v" },
+      },
+    },
+  },
+
+  {
     "mcauley-penney/visual-whitespace.nvim",
     opts = function()
       return {
