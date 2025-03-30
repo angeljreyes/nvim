a42cb84c8b1b44ce15c72117ac8e454311299bb1
DestopLine <destopline555@hotmail.com>
2025-03-30 16:41:57 -0400

    Move mini.move, Comment.nvim and nvim-surround to new motions.lua

---
+++ lua/plugins/motions.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "echasnovski/mini.move",
+    opts = {},
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+  },
+
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    opts = {},
+  },
+}
--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -2,24 +2,8 @@ return {
   "tpope/vim-sleuth",
 
   {
-    "echasnovski/mini.move",
-    opts = {},
-  },
-
-  {
-    "numToStr/Comment.nvim",
-    opts = {},
-  },
-
-  {
     "kevinhwang91/nvim-fundo",
     opts = {},
     dependencies = "kevinhwang91/promise-async",
-  },
-
-  {
-    "kylechui/nvim-surround",
-    event = "VeryLazy",
-    opts = {},
   },
 }
