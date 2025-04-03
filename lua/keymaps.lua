d39f5d9771f57c27303ee58bed631f7de075d7c2
DestopLine <destopline555@hotmail.com>
2025-04-03 18:36:22 -0400

    Revert "Use virtual lines instead of diagnostics float"
    This reverts commit 44741e04427e3e8c2b5e8b18f89d8ac190e8255d.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -17,36 +17,21 @@ vim.keymap.set({ "n", "v" }, "Z", "zz", { desc = "Center this line" })
 -- Ctrl backspace as ctrl w in insert mode
 vim.keymap.set("i", "<c-bs>", "<c-w>", { desc = "Delete word" })
 
-local function show_virtual_lines()
-  vim.diagnostic.config({
-    virtual_lines = { current_line = true },
-    virtual_text = false,
-  })
-  vim.api.nvim_create_autocmd("CursorMoved", {
-    group = vim.api.nvim_create_augroup("line-diagnostics", { clear = true }),
-    once = true,
-    callback = function()
-      vim.diagnostic.config({
-        virtual_lines = false,
-        virtual_text = true,
-      })
-    end,
-  })
-end
-
----@param count integer
-local function diagnostic_jump_virtual_lines(count)
-  vim.diagnostic.jump({ count = count })
-  vim.schedule(show_virtual_lines)
-end
-
 vim.keymap.set(
   "n",
   "[d",
-  function() diagnostic_jump_virtual_lines(-1) end,
+  function() vim.diagnostic.jump({ count = 1, float = true }) end,
   { desc = "Go to previous diagnostic message" }
 )
-
-vim.keymap.set("n", "]d", function() diagnostic_jump_virtual_lines(1) end, { desc = "Go to next diagnostic message" })
-
-vim.keymap.set("n", "<leader>e", show_virtual_lines, { desc = "Open diagnostic messages" })
+vim.keymap.set(
+  "n",
+  "]d",
+  function() vim.diagnostic.jump({ count = 1, float = true }) end,
+  { desc = "Go to next diagnostic message" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>e",
+  function() vim.diagnostic.open_float() end,
+  { desc = "Open floating diagnostic message" }
+)
