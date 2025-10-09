eba0e1856d1b08ea2c348ac3527834cd2ccf7681
DestopLine <destopline555@hotmail.com>
2025-10-08 20:52:20 -0400

    Use roslyn and netcoredbg through easy-dotnet.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,32 +33,6 @@ return {
   },
 
   {
-    "seblyng/roslyn.nvim",
-    dependencies = {
-      "tris203/rzls.nvim",
-      "saghen/blink.cmp",
-    },
-    config = function()
-      local rzls_path = vim.fn.expand("$MASON/packages/rzls/libexec")
-      vim.lsp.config("roslyn", {
-        cmd = {
-          "roslyn",
-          "--stdio",
-          "--logLevel=Information",
-          "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-          "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
-          "--razorDesignTimePath="
-            .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
-          "--extension",
-          vim.fs.joinpath(rzls_path, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll"),
-        },
-        handlers = require("rzls.roslyn_handlers"),
-      })
-      vim.lsp.enable("roslyn")
-    end,
-  },
-
-  {
     "tris203/rzls.nvim",
     ft = { "cs", "razor" },
     opts = function()
